babe928151cb5978525a7e104b9bec279d75f1c4
Justin <hsinghan1102@gmail.com>
2018-05-14 20:02:21 +0800

    Update credit.c

--- credit.c
+++ credit.c
@@ -22,18 +22,18 @@ int main()
 
 	do
 	{
-		ccn = get_long_long("Credit Card Number: ");
+	  ccn = get_long_long("Credit Card Number: ");
 	} while (ccn < 0);
 
 	if (CardLengthIsValid(ccn))
 	{
-		CheckCardVender(ccn);
+	    CheckCardVender(ccn);
 	}
 	else
-  {
-    	printf("INVALID\n");
-		  return 0;
-  } 
+        {
+    	    printf("INVALID\n");
+  	    return 0;
+        } 
     
 	return 0;
 }
@@ -42,10 +42,10 @@ bool CardLengthIsValid(long long ccn){
 	int n = (int)log10(ccn)+1;
 	if (n == 13 || n == 15 || n == 16)
 	{
-		return true;
+	   return true;
 	}
 	else
-		 return false;
+	    return false;
 }
 
 void CheckCardVender(long long n){
@@ -55,43 +55,43 @@ void CheckCardVender(long long n){
 
 	for ( card_number = n; card_number > 0; card_number /= 100)
 	{
-		sum1 += card_number%10;
+	     sum1 += card_number%10;
 	}
 
 	for (res1 = n/10 ; res1 > 0; res1 /= 100)
 	 {
-	 	if ((2 * (res1%10)) > 9)
+	     if ((2 * (res1%10)) > 9)
 	 	{
-	 		sum2 += (2 * (res1%10)) / 10;
-	 		sum2 += (2 * (res1%10)) % 10;
+	           sum2 += (2 * (res1%10)) / 10;
+	 	   sum2 += (2 * (res1%10)) % 10;
 	 	}
 	 	else
-	 		sum2 += (2 * (res1%10));
+	 	    sum2 += (2 * (res1%10));
 	 }
 
-	  total_sum = sum1 + sum2;
+	total_sum = sum1 + sum2;
    
    // check the card number range
 	if (total_sum % 10 == 0)
   	{
-       if ( (n >= 340000000000000 && n < 350000000000000) || (n >= 370000000000000 && n < 380000000000000))
-       {
-           printf("AMERICA EXPRESS\n");
-       }
-       else if (n >= 5100000000000000 && n < 5600000000000000)
-       {
-           printf("MASTERCARD\n");
-       }
-       else if ((n >= 4000000000000 && n < 5000000000000)|| (n >= 4000000000000000 && n < 5000000000000000))
-       {
-           printf("VISA\n");
-       }
-       else
-         printf("VALID\n");
-
+           if ( (n >= 340000000000000 && n < 350000000000000) || (n >= 370000000000000 && n < 380000000000000))
+           {
+              printf("AMERICA EXPRESS\n");
+           }
+           else if (n >= 5100000000000000 && n < 5600000000000000)
+           {
+              printf("MASTERCARD\n");
+           }
+           else if ((n >= 4000000000000 && n < 5000000000000)|| (n >= 4000000000000000 && n < 5000000000000000))
+           {
+              printf("VISA\n");
+            }
+           else
+              printf("VALID\n");
+		
   	 }
  	 else
-  	   printf("INVALID\n");
+  	    printf("INVALID\n");
 }
 
 
